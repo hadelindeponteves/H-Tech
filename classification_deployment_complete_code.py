83c66e527025c73776c9dd2185b0103fb41a17fc
Hadelin de Ponteves <39771260+hadelindeponteves@users.noreply.github.com>
2026-09-08 19:04:51 +0200

    Update classification_deployment_complete_code.py

--- classification_deployment_complete_code.py
+++ classification_deployment_complete_code.py
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 
 # Defining a conservative classification threshold
-CLASSIFICATION_THRESHOLD = 0.70
+CLASSIFICATION_THRESHOLD = 0.65
 
 # Applying the H-Tech visual identity
 st.markdown(
